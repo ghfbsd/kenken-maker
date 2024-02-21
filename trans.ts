f6ff608c6fd2e494c05376fbbd9c63b333d9419a
ghfbsd <ghfbsd@gmail.com>
2024-02-21 10:40:18 +0000

    Simplify code, replace promise.all use

--- trans.ts
+++ trans.ts
@@ -18,39 +18,34 @@ const readPuzzle = new Promise<Puzzle>((resolve, reject) => {
 		else resolve(value!)
 	})
 })
-Promise.all([readPuzzle])
-	.then(([puzzle]) => {
-		// Output the puzzle in readable format
-		const ltr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop'
-		const {max, cages} = puzzle!
-		let grid: string[] =
-			[...Array(max).keys()].map(() => '.'.repeat(max))
-		let n = -1
-		for(const cage of cages) {
-			if (cage.boxes.length > 1) n++
-			for(const box of cage.boxes) {
-				const [r, c] = box
-				if (cage.op === '=') {
-					grid[r] = grid[r].substr(0,c) +
-					   cage.val +
-					   grid[r].substr(c+1)
-					break
-				}
-				const chr = ltr.substr(n,1)
-				grid[r] = grid[r].substr(0,c) + chr +
-					grid[r].substr(c+1)
-			}
+
+readPuzzle.then((puzzle) => {
+	// Output the puzzle in readable format
+	const ltr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop'
+	const {max, cages} = puzzle!
+	let grid: string[] =
+		[...Array(max).keys()].map(() => '.'.repeat(max))
+	let n = -1
+	for(const cage of cages) {
+		if (cage.boxes.length > 1) n++
+		for(const box of cage.boxes) {
+			const [r, c] = box
+			const chr = (cage.op === '=') ?
+				String(cage.val) : ltr.substr(n,1)
+			grid[r] = grid[r].substr(0,c) + chr +
+				grid[r].substr(c+1)
 		}
-		process.stdout.write('.KK "' + argv[2] + '"\n')
-		grid.map((s) => process.stdout.write(s + '\n'))
-		process.stdout.write('\n')
-		n = 0
-		for(const cage of cages) {
-			if (cage.boxes.length === 1) continue
-			const chr = ltr.substr(n,1)
-			let op = (cage.op !== '*') ? cage.op : 'x'
-			process.stdout.write(chr + ' ' + cage.val + op + '\n')
-			n++
-		}
-	})
-	.catch(console.error)
+	}
+	process.stdout.write('.KK "' + argv[2] + '"\n')
+	grid.map((s) => process.stdout.write(s + '\n'))
+	process.stdout.write('\n')
+	n = 0
+	for(const cage of cages) {
+		if (cage.boxes.length === 1) continue
+		const chr = ltr.substr(n,1)
+		let op = (cage.op !== '*') ? cage.op : 'x'
+		process.stdout.write(chr + ' ' + cage.val + op + '\n')
+		n++
+	}
+})
+.catch(console.error)
